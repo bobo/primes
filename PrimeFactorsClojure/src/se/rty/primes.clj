589e3f6f0e7a803a6dd927104303204f04e76e52
Mikael Sundberg <mikael.sundberg82@gmail.com>
2010-05-14 23:26:14 +0200

    first version

---
+++ PrimeFactorsClojure/src/se/rty/primes.clj
@@ -0,0 +1,27 @@
+(comment
+ Sample clojure source file
+ )
+(ns se.rty.primes
+  (:gen-class))
+
+(defn find-primes [num divisor primes]
+  (if (> num 1)
+    (if (= (mod num divisor) 0)
+      (recur (/ num divisor) divisor (concat primes (list divisor)))
+      (if (> num 0)
+        (recur num (+ 1 divisor) primes)
+      (list num )))
+  primes))
+
+(defn primes [num]
+  (nils num 2 (list)))
+
+
+
+
+
+
+(defn -main
+  ([greetee]
+    (println (str "Hello " greetee "!")))
+  ([] (-main "world")))
---
+++ PrimeFactorsClojure/src/se/rty/primes_test.clj
@@ -0,0 +1,31 @@
+
+(ns se.rty.primes
+  (:require clojure.test)
+  ;(:import )
+  )
+(use 'clojure.test)
+
+(deftest primes-test
+  (are [number factors]
+    (= (primes number) factors)
+    1 (list),
+    2 (list 2),
+    3 (list 3),
+    4 (list 2 2),
+    5 (list 5),
+    6 (list 2 3),
+    7 (list 7),
+    8 (list 2 2 2),
+    9 (list 3 3),
+    10 (list 2 5),
+    11 (list 11),
+    12 (list 2 2 3),
+    13 (list 13),
+    12345 (list 3 5 823),
+    10000 (list 2 2 2 2 5 5 5 5),
+    100000 (list 2 2 2 2 2 5 5 5 5 5),
+    677329 (list 823 823)))
+
+
+
+(run-tests)
